4d3a842f50682dc610750ea4f72cdb2065860c27
Mohit Khotani <mohit.khotani@gmail.com>
2020-04-22 22:00:56 +0530

    intial setup of mongo, react and nodejs

---
+++ backend/database.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+
+const url = "";
+
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
